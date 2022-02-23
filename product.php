1397a48e231c891f4381371581c4ce56010026d3
David E Luna M <davidlunamontilla@gmail.com>
2022-02-23 02:37:41 -0400

    Se mejora el formato JSON de los productos

--- product.php
+++ product.php
@@ -5,19 +5,25 @@ $phones = [
     [
         "product_title" => "iPhone 7",
         "product_description" => "Descripción del producto",
-        "product_image" => "",
-        "product_price" => 0,
-        "product_color" => "color del producto",
-        "product_money" => "USD"
+
+        "attributes" => [
+            "image" => "",
+            "price" => 0,
+            "color" => "",
+            "money" => "USD"
+        ]
     ],
 
     [
         "product_title" => "Samsung Galaxy S7",
         "product_description" => "Descripción del producto",
-        "product_image" => "",
-        "product_price" => 0,
-        "product_color" => "color del producto",
-        "product_money" => "USD"
+
+        "attributes" => [
+            "image" => "",
+            "price" => 0,
+            "color" => "",
+            "money" => "USD"
+        ]
     ]
 ];
 
